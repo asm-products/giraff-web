2701bb07abdc658e7105614e9b0e97be114f2b04
Mikhail Fedosov <tbs.micle@gmail.com>
2015-02-27 00:22:31 +0300

    Implement "/v/SHORT_CODE" view

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ eventlet.monkey_patch()
 
 
 import os
+import requests
 from whitenoise import WhiteNoise
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 
 BASE_DIR = os.path.dirname(__file__)
+API_ENDPOINT = os.getenv("API_ENDPOINT", "http://fun-api.herokuapp.com/shortcode/")
 
 
 app = Flask(__name__, instance_relative_config=True)
@@ -18,6 +20,14 @@ app.wsgi_app = WhiteNoise(app.wsgi_app, root=os.path.join(BASE_DIR, "assets"), p
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/v/<shortcode>")
+def view(shortcode):
+    api_url = "%(API_ENDPOINT)s%(shortcode)s.json" % { "API_ENDPOINT": API_ENDPOINT, "shortcode": shortcode }
+    data = requests.get(api_url).json()
+    if not data:
+        return redirect("/")
+    return render_template("view.html", shortcode=shortcode, data=data, host_url=request.host_url)
 
 @app.route("/thank-you.html")
 def thank_you():
