354e6b6a449016a0841e62e8c9107bb3cd9d9ce2
Mikhail Fedosov <tbs.micle@gmail.com>
2015-02-27 01:13:44 +0300

    JS cleanup

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,30 +1,5 @@
-var API_ENDPOINT = "http://fun-api.herokuapp.com/shortcode/";
-
 jQuery(function($)
 {
-    var hash = window.location.hash;
-    if (hash) 
-    {
-        $.ajax(
-        {
-            url: API_ENDPOINT + hash.substr(1) + ".json",
-            type: "get",
-            error: function()
-            {
-                console.log("Error: gif not found.");
-            },
-            success: function(data)
-            {
-                $("#main").remove();
-                $(".spinner").remove();
-                $("#image-share-link").text(window.location);
-                $("#image-caption").text(data.name);
-                $("#image-view").attr("src", data.original_source);
-                $("#shortcode").show();
-            }
-        });
-    }
-
     $(".slider-list li:first-child").on("click", function()
     {
         $(".focus_me_please").effect("highlight").focus();
