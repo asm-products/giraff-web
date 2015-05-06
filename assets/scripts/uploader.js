725042f54cf1002ee50359b21f3ede6e71c062c4
Pablo Villoslada Puigcerber <pablo85@gmail.com>
2015-05-06 17:54:32 +0200

    Convert image to Base64 and send request to the backend

--- assets/scripts/uploader.js
+++ assets/scripts/uploader.js
@@ -31,12 +31,60 @@ jQuery(function($) {
 
     function handleFiles(fileList) {
         for (var i = 0, len = fileList.length; i < len; i++) {
-            files.push(fileList[i]);
+            convertImage(fileList[i]);
         }
     }
 
+    function convertImage(file) {
+        var fileReader = new FileReader();
+
+        var image = {
+            "bytes": "",
+            "filename": file.name,
+            "content": "",
+            "content_type": file.type
+        };
+
+        fileReader.onload = function(e) {
+            image.bytes = e.target.result.length;
+            image.content = e.target.result;
+            files.push(image);
+        };
+
+        fileReader.readAsDataURL(file);
+    }
+
     function doUpload() {
-        // Wire to Flask.
+        var imageRequest = {
+            "image": {
+                "name": "",
+                "original_source": ""
+            }
+        };
+
+        var originalSource = $("#upload-source").val();
+
+        if (originalSource) {
+            imageRequest.image.original_source = originalSource;
+        } else if (files.length > 0) {
+            imageRequest.image.bytes = files[0].bytes;
+            imageRequest.image.file = {
+                "filename": files[0].filename,
+                "content": files[0].content,
+                "content_type": files[0].content_type
+            };
+        } else {
+            return;
+        }
+
+        $.ajax({
+            url: 'http://fun-api.herokuapp.com/images',
+            method: 'POST',
+            data: imageRequest,
+            success: function(data) {
+                console.log(data);
+            }
+        })
     }
 
 });
